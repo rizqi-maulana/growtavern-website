44142bf21e4c998104f5cee4fd921b9cdfefcaff
Maulanya <maulanarizq26@gmail.com>
2024-09-12 11:53:55 +0800

    fix: signup notification and created account validate

--- src/components/template/signup.tsx
+++ src/components/template/signup.tsx
@@ -4,6 +4,7 @@ import { useState, useContext, useCallback, useRef, FormEvent } from "react";
 import { UserContext } from "@/context";
 import FormSignUp from "../organisms/FormSignUp";
 import Confetti, { ConfettiRef } from "../magicui/confetti ";
+import toast, { Toaster } from 'react-hot-toast';
 
 function SignUp() {
   const confettiRef = useRef<ConfettiRef>(null);
@@ -14,7 +15,7 @@ function SignUp() {
     throw new Error("UserContext must be used within a UserProvider");
   }
 
-  const { Name, Password, Email, Gender } = context;
+  const { Name, Password, Email, Gender, setSignUpForm } = context;
   const [ShowSuccess, setShowSuccess] = useState<boolean>(false);
 
   // Menangani pengiriman formulir
@@ -22,7 +23,7 @@ function SignUp() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://143.110.178.133:1515", {
+      const res = await fetch("http://localhost:1515/create/growid", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -34,10 +35,21 @@ function SignUp() {
           gender: Gender
         }),
       });
+      const data = await res.json()
 
-      if (res.ok) {
-        setShowSuccess(true);
-        confettiRef.current?.fire();
+      if (data.type === "account_created") {
+        toast.success(data.message);
+        await setShowSuccess(true);
+        await confettiRef.current?.fire();
+        setTimeout(() => {
+          setSignUpForm(false)
+        }, 3000);
+      }
+      if (data.type === "account_exist") {
+        toast.error(data.message);
+      }
+      if (data.type === "server_error") {
+        toast.error(data.message);
       }
     } catch (error) {
       console.error("Error during signup:", error);
@@ -47,18 +59,21 @@ function SignUp() {
 
 
   return (
-    <form
-      className="w-full h-screen fixed backdrop-blur-lg bg-black/50 z-[99] flex flex-col justify-center"
-      onSubmit={HandleSignUp}
-    >
-      <FormSignUp />
-      {ShowSuccess && (
-        <Confetti
-          ref={confettiRef}
-          className="absolute left-0 top-0 z-0 size-full"
-        />
-      )}
-    </form>
+    <>
+      <Toaster />
+      <form
+        className="w-full h-screen fixed backdrop-blur-lg bg-black/50 z-[99] flex flex-col justify-center"
+        onSubmit={HandleSignUp}
+      >
+        <FormSignUp />
+        {ShowSuccess && (
+          <Confetti
+            ref={confettiRef}
+            className="absolute left-0 top-0 z-0 size-full"
+          />
+        )}
+      </form>
+    </>
   );
 }
 
