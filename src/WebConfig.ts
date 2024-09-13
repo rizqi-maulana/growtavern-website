b12d278f663436ccdb7048d47170e6630cc9625e
Maulanya <maulanarizq26@gmail.com>
2024-09-13 13:48:26 +0800

    style: added some interface to web config

--- src/WebConfig.ts
+++ src/WebConfig.ts
@@ -1,3 +1,7 @@
-export const WebConfig = {
+interface WebConfigProps {
+  logo: string
+}
+
+export const WebConfig: WebConfigProps = {
   logo: "https://res.cloudinary.com/dju3jontk/image/upload/q_100/v1726205113/growtavern_i0uro3.webp"
 }
