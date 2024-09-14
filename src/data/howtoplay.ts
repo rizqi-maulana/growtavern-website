2159238f9f6b3e36950fbb3eb6476cd28edff22d
Maulanya <maulanarizq26@gmail.com>
2024-09-14 16:03:51 +0800

    style: rename file host

--- src/data/howtoplay.ts
+++ src/data/howtoplay.ts
@@ -35,7 +35,7 @@ C:\Windows\System32\drivers\etc in the textbox.
     Description: "Play on the GrowTavern Server with a Desktop Device",
     Icon: 'https://res.cloudinary.com/dju3jontk/image/upload/q_100/v1726022035/cell-phone_ukiys9.webp',
     Image: 'https://res.cloudinary.com/dju3jontk/image/upload/q_100/v1726023034/Characters_wsvcpp.webp',
-    File: "/ubstopia.txt",
+    File: "/growtavern.txt",
     Content: `Play GrowTavern on Android
     • Download Virtual Host In Playstore
 • Download UBS TOPIA file at below
