a71557c3a32a0d68af5874b51ae76930f141b207
Maulanya <maulanarizq26@gmail.com>
2024-09-25 21:29:41 +0800

    feat: add anti ddos

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,25 @@
+// middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { RateLimiterMemory } from 'rate-limiter-flexible';
 
-export function middleware(request: NextRequest) {
-  const url = request.nextUrl;
+const rateLimiter = new RateLimiterMemory({
+  points: 10,
+  duration: 0.5,
+});
 
-  if (url.pathname === '/store/') {
-    return NextResponse.redirect(new URL('/', request.url));
+export async function middleware(req: NextRequest) {
+  try {
+    const ip = req.headers.get('x-forwarded-for') || req.ip || '127.0.0.1';
+
+    await rateLimiter.consume(ip);
+
+    return NextResponse.next();
+  } catch (rejRes) {
+    return new NextResponse('Too Many Requests', { status: 429 });
   }
-
-  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/store/:path*'],
+  matcher: '/:path*',
 };
