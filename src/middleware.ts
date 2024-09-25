ef30a2ba16273f98287efbb3a7505163bfdb8796
Maulanya <maulanarizq26@gmail.com>
2024-09-25 22:26:28 +0800

    fix: disabled middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,25 +1,25 @@
-// middleware.ts
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-import { RateLimiterMemory } from 'rate-limiter-flexible';
+// // middleware.ts
+// import { NextResponse } from 'next/server';
+// import type { NextRequest } from 'next/server';
+// import { RateLimiterMemory } from 'rate-limiter-flexible';
 
-const rateLimiter = new RateLimiterMemory({
-  points: 10,
-  duration: 60,
-});
+// const rateLimiter = new RateLimiterMemory({
+//   points: 10,
+//   duration: 0.5,
+// });
 
-export async function middleware(req: NextRequest) {
-  try {
-    const ip = req.headers.get('x-forwarded-for') || req.ip || '127.0.0.1';
+// export async function middleware(req: NextRequest) {
+//   try {
+//     const ip = req.headers.get('x-forwarded-for') || req.ip || '127.0.0.1';
 
-    await rateLimiter.consume(ip);
+//     await rateLimiter.consume(ip);
 
-    return NextResponse.next();
-  } catch (rejRes) {
-    return new NextResponse('Too Many Requests', { status: 429 });
-  }
-}
+//     return NextResponse.next();
+//   } catch (rejRes) {
+//     return new NextResponse('Too Many Requests', { status: 429 });
+//   }
+// }
 
-export const config = {
-  matcher: '/:path*',
-};
+// export const config = {
+//   matcher: '/:path*',
+// };
