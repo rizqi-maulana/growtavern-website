981094bfcd1d1e144a59adb6b3eb02f4c10f0fd8
Maulanya <maulanarizq26@gmail.com>
2024-09-29 20:50:56 +0800

    feat: data for Developed slider

---
+++ src/data/developed.ts
@@ -0,0 +1,14 @@
+interface Item {
+  title: string;
+  image: string;
+  desc: string;
+}
+
+export const DevelopedData: Item[] = [
+
+  {
+    title: "Growtopia x Minecraft Custom Items",
+    image: "https://res.cloudinary.com/dju3jontk/image/upload/q_100/v1727612171/Tavern_basic_udtavj.webp",
+    desc: "This is a custom set of items created for our Growtopia private server, blending the styles of Growtopia and Minecraft. It features various tools, chests, ores, and crafting materials inspired by Minecraft, but designed with a Growtopia aesthetic. These items enhance the gameplay experience by introducing new mechanics and visuals unique to our server, offering players a creative and immersive fusion of two popular games."
+  }
+]
